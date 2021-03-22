f1d077e0e346ce97b57e6a9cb56e2debdbe1af3e
thorcc <thorchrcow@gmail.com>
2021-03-22 19:54:04 +0100

    remove description

--- src/templates/picture.js
+++ src/templates/picture.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql } from 'gatsby'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import Layout from "../components/layout"
 import classes from "../styles/picture.module.css"
@@ -12,10 +11,6 @@ export default function Picture({ data }) {
       <div className={classes.picture}>
         <h1>{pic.title}</h1>
         <img src={pic.picture.file.url} alt={pic.title} />
-        {pic.description ? 
-        <div className={classes.text}>
-          {documentToReactComponents(pic.description.json)}
-        </div> : ""}
       </div>
     </Layout>
   )
@@ -25,9 +20,6 @@ export const query = graphql`
     query($slug: String!) {
         contentfulPicture(slug: {eq: $slug}) {
             title
-            description {
-              json
-            }
             picture {
               file {
                 url
